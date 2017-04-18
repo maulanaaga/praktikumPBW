e3a65595ab8e615e2d0156f5a35f5e0feb7b4b5c
maulanaaga <agamaulana.informatika@gmail.com>
2017-04-18 13:59:38 +0700

    Materi praktikum untuk show data yang ada pada database setelah di add melalui add.php

---
+++ praktikum4/database/data.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 11, 2017 at 07:41 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `nik` varchar(10) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `tempat_lahir` varchar(30) NOT NULL,
+  `tanggal_lahir` varchar(30) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `no_telepon` varchar(12) NOT NULL,
+  `jabatan` varchar(15) NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+INSERT INTO `karyawan` (`nik`, `nama`, `tempat_lahir`, `tanggal_lahir`, `alamat`, `no_telepon`, `jabatan`, `status`, `username`, `password`) VALUES
+('1', 'reki', 'Banda Aceh', '2017-04-03', 'banda', '08212121', 'Operator', 'Kontrak', 'rekii', '202cb962ac59075'),
+('2', 'agus', 'Banda Aceh', '2017-04-06', 'Banda', '081211131', 'Leader', 'Kontrak', 'agus11', 'fc159b15068b3ff');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`nik`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ praktikum4/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "data";
+
+$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("Gagal Melakukan Koneksi ke Database");
+
+?>
